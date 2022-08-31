ad3f70176c2a0c692a97dddef82333b7897350de
Chris Mann <chrispmann@gmail.com>
2022-08-31 14:53:26 -0700

    commit tree 2b3dfe67a5cfc1e990de11f20fb80fcb00a14efb

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+function addingEventListener(){
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
+
 }
+
